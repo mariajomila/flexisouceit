a335e6d1c3dd2d1f4d7126ce7d6756a1bbac50f5
Maria Flores <mariaf@excelaustralasia.com.au>
2020-03-07 19:35:39 +1100

    Coding exam answer.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using opg_201910_interview.Models;
-
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Text.Json;
 namespace opg_201910_interview.Controllers
 {
     public class HomeController : Controller
@@ -20,6 +22,33 @@ namespace opg_201910_interview.Controllers
 
         public IActionResult Index()
         {
+            var fileLocation = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ClientSettings")["FileDirectoryPath"];
+            var fixedOrder = new List<string>();
+            string[] files = Directory.GetFiles(fileLocation);
+            List<XMLInfo> c = new List<XMLInfo>();
+            if (fileLocation.Contains("ClientA"))
+            {
+                fixedOrder = new List<string> { "shovel", "waghor", "blaze", "discus" };
+            }
+            else if (fileLocation.Contains("ClientB"))
+            {
+                fixedOrder = new List<string> { "orca", "widget", "eclair", "talon" };
+            }
+            foreach (string file in files)
+            {
+                if (fixedOrder.Count() > 0 && fixedOrder.Any(file.Contains))
+                {
+                    XMLInfo n = new XMLInfo();
+                    string fileName = file.Replace(@"\", "/").Replace(fileLocation + "/", "");
+                    n.OrderNumber = GetOrderNumber(fixedOrder, fileName);
+                    n.Filename = fileName;
+                    c.Add(n);
+                }
+            }
+            var result = c.OrderBy(c => c.OrderNumber).ThenBy(c => c.Filename);
+            var json = JsonSerializer.Serialize(result.Select(w => w.Filename));
+            ViewData["Filenames"] = json;
+
             return View();
         }
 
@@ -32,6 +61,21 @@ namespace opg_201910_interview.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public class XMLInfo
+        {
+            public string Filename { get; set; }
+            public int OrderNumber { get; set; }
+        }
+        private static int GetOrderNumber(List<string> stringOrder, string fileName)
+        {
+            int length = stringOrder.Count();
+            for (int index = 0; index < length; index++)
+            {
+                if (fileName.Contains(stringOrder[index]))
+                    return index;
+            }
+            return 0;
         }
     }
 }
